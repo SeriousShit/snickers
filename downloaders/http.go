cfaaee9fe86a0e488a0af58af2361d04bac91973
Hagen <hagen@serious-shit.de>
2017-08-13 16:13:45 +0200

    remove deprecated methods of grab
    replacing  grab.GetAsync with grab.NewRequest

--- downloaders/http.go
+++ downloaders/http.go
@@ -2,6 +2,7 @@ package downloaders
 
 import (
 	"strconv"
+	"time"
 
 	"code.cloudfoundry.org/lager"
 
@@ -9,6 +10,48 @@ import (
 	"github.com/flavioribeiro/gonfig"
 	"github.com/snickers/snickers/db"
 )
+
+//// HTTPDownload function downloads sources using
+//// http protocol.
+//func HTTPDownload(logger lager.Logger, config gonfig.Gonfig, dbInstance db.Storage, jobID string) error {
+//	log := logger.Session("http-download")
+//	log.Info("start", lager.Data{"job": jobID})
+//	defer log.Info("finished")
+//
+//	job, err := dbInstance.RetrieveJob(jobID)
+//	if err != nil {
+//		return err
+//	}
+//
+//	respch, err := grab.GetAsync(job.LocalSource, job.Source)
+//	if err != nil {
+//		return nil
+//	}
+//
+//	resp := <-respch
+//	for !resp.IsComplete() {
+//		job, err = dbInstance.RetrieveJob(jobID)
+//		if err != nil {
+//			return err
+//		}
+//
+//		percentage := strconv.FormatInt(int64(resp.BytesTransferred()*100/resp.Size), 10)
+//		if job.Progress != percentage {
+//			job.Progress = percentage + "%"
+//			dbInstance.UpdateJob(job.ID, job)
+//		}
+//	}
+//
+//	if resp.Error != nil {
+//		return resp.Error
+//	}
+//
+//	return nil
+//
+//
+//
+//}
+
 
 // HTTPDownload function downloads sources using
 // http protocol.
@@ -22,24 +65,43 @@ func HTTPDownload(logger lager.Logger, config gonfig.Gonfig, dbInstance db.Stora
 		return err
 	}
 
-	respch, err := grab.GetAsync(job.LocalSource, job.Source)
+
+	client := grab.NewClient()
+	req, err := grab.NewRequest(job.LocalSource, job.Source)
+
+	resp := client.Do(req)
+
+	t := time.NewTicker(500 * time.Millisecond)
+	defer t.Stop()
+
+	for {
+		select {
+		case <-t.C:
+			job, err = dbInstance.RetrieveJob(jobID)
+			if err != nil {
+				return err
+			}
+
+			percentage := strconv.FormatInt(int64(resp.Progress()), 10)
+
+			if job.Progress != percentage {
+				job.Progress = percentage + "%"
+				dbInstance.UpdateJob(job.ID, job)
+			}
+
+		case <-resp.Done:
+			// download is complete
+			break Loop
+		}
+	}
+
+
+	//respch, err := grab.GetAsync(job.LocalSource, job.Source)
 	if err != nil {
 		return nil
 	}
 
-	resp := <-respch
-	for !resp.IsComplete() {
-		job, err = dbInstance.RetrieveJob(jobID)
-		if err != nil {
-			return err
-		}
 
-		percentage := strconv.FormatInt(int64(resp.BytesTransferred()*100/resp.Size), 10)
-		if job.Progress != percentage {
-			job.Progress = percentage + "%"
-			dbInstance.UpdateJob(job.ID, job)
-		}
-	}
 
 	if resp.Error != nil {
 		return resp.Error
